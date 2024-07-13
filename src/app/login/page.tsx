ed286e103b3457eb209fd0627cf5d9435dd87e33
themuuln <zerone.offical@gmail.com>
2024-07-14 04:00:52 +0800

    ui updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { supabase } from '@/lib/initSupabase';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -29,23 +32,19 @@ export default function Login() {
   };
 
   return (
-    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+    <div className='flex justify-center items-center py-24'>
       <div className='w-full max-w-md'>
         <form
           onSubmit={handleSubmit}
-          className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+          className='shadow-md rounded px-8 pt-6 pb-8 mb-4'
         >
           <h2 className='text-2xl mb-6 text-center font-bold'>Login</h2>
           {error && <p className='text-red-500 text-xs italic mb-4'>{error}</p>}
           <div className='mb-4'>
-            <label
-              className='block text-gray-700 text-sm font-bold mb-2'
-              htmlFor='email'
-            >
+            <label className='block text-sm font-bold mb-2' htmlFor='email'>
               Email
             </label>
-            <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            <Input
               id='email'
               type='email'
               placeholder='Email'
@@ -55,35 +54,23 @@ export default function Login() {
             />
           </div>
           <div className='mb-6'>
-            <label
-              className='block text-gray-700 text-sm font-bold mb-2'
-              htmlFor='password'
-            >
+            <label className='block text-sm font-bold mb-2' htmlFor='password'>
               Password
             </label>
-            <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+            <Input
               id='password'
               type='password'
-              placeholder='******************'
+              placeholder='Password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
           <div className='flex items-center justify-between'>
-            <button
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-              type='submit'
-            >
-              Sign In
-            </button>
-            <a
-              className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800'
-              href='/register'
-            >
-              Need an account?
-            </a>
+            <Button type='submit'>Sign In</Button>
+            <Link href={'/register'}>
+              <Button variant={'link'}>Need an account?</Button>
+            </Link>
           </div>
         </form>
       </div>
